b61c900f92843b7537660e1b2fa70f2b254bad41
shun04101 <nakashimashuntarou@gmail.com>
2022-03-31 20:32:48 +0900

    [Add]基礎編9章(クラス)

---
+++ src/Human02.java
@@ -0,0 +1,17 @@
+
+public class Human02 {
+	public String name; //メンバー変数
+	public int age; //メンバー変数
+
+	//引数なしのコンストラクタ
+	public Human02() {
+        name = "山田";
+        age = 20;
+    }
+
+	//引数2つのコンストラクタ
+	public Human02(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
