2ed7915f4d56717a04cb39ed4ca1af44c8111666
shun04101 <nakashimashuntarou@gmail.com>
2022-03-31 21:00:26 +0900

    [Update]基礎編9章(メソッド)

--- src/ClassMethod02.java
+++ src/ClassMethod02.java
@@ -3,10 +3,10 @@ public class ClassMethod02 {
 
 	public static void main(String[] args) {
 
-		Human02 yamada = new Human02(); //Human01クラスのインスタンスを生成
+		Human02 yamada = new Human02(); //Human02クラスのインスタンスを生成
 		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
 
-		Human02 sato = new Human02("佐藤", 25); //Human01クラスのインスタンスを生成
+		Human02 sato = new Human02("佐藤", 25); //Human02クラスのインスタンスを生成
 		System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
 
 	}
